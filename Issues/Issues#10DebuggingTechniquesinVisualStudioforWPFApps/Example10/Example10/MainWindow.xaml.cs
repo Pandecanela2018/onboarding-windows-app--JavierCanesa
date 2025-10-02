e621a1680a8ae64676f54f621fc9bc978f31e26b
Canesa <javier.canesa2014@gmail.com>
2025-10-02 16:42:33 -0600

    Add issues#10

---
+++ Issues/Issues#10DebuggingTechniquesinVisualStudioforWPFApps/Example10/Example10/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int value = int.Parse(NumberInput.Text);
+                int doubled = value * 2;
+                ResultText.Text = $"Result: {doubled}";
+            }
+            catch (Exception exception)
+            {
+
+                ResultText.Text = $"Algo salio mal. Detalle:  {exception.Message} ";
+            }
+
+
+            
+
+
+        }
+    }
+}
---
+++ Issues/Issues#10DebuggingTechniquesinVisualStudioforWPFApps/debugging-techniques-reflection.md
@@ -0,0 +1,19 @@
+# Reflection
+
+## Which debugging tools in Visual Studio do you find most useful and why?
+
+The tools I find most useful are the combination of breakpoints and the Watch
+Window, since they allow me to verify if variables are being updated correctly
+based on user input.
+
+## Reflect on a scenario where a specific debugging feature helped you identify and resolve an issue.
+
+In my example, the expected output was supposed to be double the input. However,
+the result was the same as the input, meaning the multiplication was not
+happening correctly. Thanks to the Watch Window, I was able to verify that the
+conversion was not working as expected and fix the issue.
+
+## How might improving your debugging skills impact your overall productivity?
+
+Improving my debugging skills would help me identify errors earlier, especially
+in conversions or function calls, and prevent them more effectively.
